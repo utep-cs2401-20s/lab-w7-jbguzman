9e939e267ad05e90492221665d297a85e962ae7b
jbguzman <jbguzman@miners.utep.edu>
2020-03-08 22:43:55 -0600

    Completed.

--- SortofSort.java/src/SortOfSort.java
+++ SortofSort.java/src/SortOfSort.java
@@ -1,10 +1,38 @@
 public class SortOfSort {
-    public static void sortOfSort(int[] array){
-        int max = 0;
-        
-        for(int i = 0; i < array.length; i++){
-            //
+    public static void sortOfSort(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        int count = 0;
+        int max;
+        int current;
+        int temp = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            max = Integer.MIN_VALUE;
+            current = i;
 
+            for (int j = left; j <= right; j++) {
+                if (array[j] > max)
+                    max = array[j];
+                    current = j;
+            }
+        }
+        if (left < right) {
+            if (count < 2) {
+                temp = array[right];
+                array[right] = max;
+                array[current] = temp;
+                count++;
+                right--;
+            } else if (count < 4) {
+                temp = array[left];
+                array[left] = max;
+                array[current] = temp;
+                count++;
+                left++;
+            }
+        }
+        if (count == 4) {
+            count = 0;
         }
     }
 }
---
+++ SortofSort.java/src/SortOfSortTester.java
@@ -0,0 +1,37 @@
+class SortOfSortTester {
+    //testing an 4 integer array
+    @Test
+    public void testSortOfSort1(){
+        int [] a = new int [];
+        a = {2, 9, 5, 1};
+        assetEquals({2, 1, 5, 9}, sortOfSort(a));
+    }
+
+    //testing an odd integer array
+    @Test
+    public void testSortOfSort2(){
+        int [] a = new int [];
+        a = {11, 0, 4, 23, 7};
+        assetEquals({7, 4, 0, 11, 23});
+    }
+
+    //test an array with negative and positive integers
+    @Test
+    public void testSortOfSort3(){
+        int [] a = new int [];
+        a = {-3, 1, -6, 10};
+        assetEquals({-3, -6, 1, 10});
+    }
+    @Test
+    public void testSortOfSort4(){
+        int [] a = new int [];
+        a = {2, 9, 5, 1};
+        assetEquals({2, 1, 5, 9});
+    }
+    @Test
+    public void testSortOfSort5(){
+        int [] a = new int [];
+        a = {2, 9, 5, 1};
+        assetEquals({2, 1, 5, 9});
+    }
+}
